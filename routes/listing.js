4756b3d820fdbedbd272252817096529cda63e29
Michael Cruz <michael.cruz@uwalumni.com>
2016-11-20 20:29:19 -0800

    consolidates routes

--- routes/listing.js
+++ routes/listing.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var Listing = require('../models/listing');
-var router = express.Router();
-var cors = require('cors');
-
-/* GET home page. */
-router.post('/', cors(), function(req, res, next) {
-  var newListing = {
-    city: req.body.city,
-    address: req.body.address,
-    zipcode: req.body.zipcode,
-    price: req.body.price,
-    bedrooms: req.body.bedrooms,
-    bathrooms: req.body.bathrooms,
-    duration: req.body.duration,
-    pets: req.body.pets,
-    furnished: req.body.furnished
-  }
-  console.log(newListing);
-  Listing.create(newListing, function(err, listing) {
-    if (err) res.send(err);
-    res.json(listing);
-  });
-});
-
-module.exports = router;
+// var express = require('express');
+// var Listing = require('../models/listing');
+// var router = express.Router();
+// var cors = require('cors');
+//
+// /* GET home page. */
+// router.post('/', cors(), function(req, res, next) {
+//   var newListing = {
+//     city: req.body.city,
+//     address: req.body.address,
+//     zipcode: req.body.zipcode,
+//     price: req.body.price,
+//     bedrooms: req.body.bedrooms,
+//     bathrooms: req.body.bathrooms,
+//     duration: req.body.duration,
+//     pets: req.body.pets,
+//     furnished: req.body.furnished
+//   }
+//   console.log(newListing);
+//   Listing.create(newListing, function(err, listing) {
+//     if (err) res.send(err);
+//     res.json(listing);
+//   });
+// });
+//
+// module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('./config/database');
 var index = require('./routes/index');
 var listings = require('./routes/listings')
-var listing = require('./routes/listing');
+// var listing = require('./routes/listing');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/listings', listings);
-app.use('/listing', listing);
+// app.use('/listing', listing);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
